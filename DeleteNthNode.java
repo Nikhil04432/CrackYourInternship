bbb060b5f76e6e0f1bdf74db23b410bd0d4b21b7
Nikhil04432 <gilnikhil15@gmail.com>
2024-10-08 20:26:23 +0530

    8/10

---
+++ DeleteNthNode.java
@@ -0,0 +1,26 @@
+
+public class DeleteNthNode {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        //O(L) traverse only once;
+         ListNode dummy = new ListNode(1);
+        dummy.next = head;
+        
+        ListNode front = dummy;
+        ListNode back = dummy;
+        
+        for(int i=0; i<=n; i++){
+            front = front.next;
+        }
+        
+        while(front != null){
+            front = front.next;
+            back = back.next;
+        }
+        
+        back.next = back.next.next;
+        
+        return dummy.next;
+
+    }
+}
